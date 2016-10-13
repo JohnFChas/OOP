3ef586c3e97e0f2dfebb41df211040faf3e11c6d
John Fristedt <john.fristedt@chas.se>
2016-10-13 11:15:31 +0200

    Workshop Arv och Polymorfism

---
+++ Workshops/Workshop - ArvPolymorfism/ArvPolymorfsim/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArvPolymorfsim
+{
+    public class Teacher : Person
+    {
+        public string Subject { get; set; }
+
+        public override string Introduction()
+        {
+            return String.Format("{0} Jag undervisar i {1}", 
+                                 base.Introduction(),
+                                 Subject);
+        }
+    }
+}
